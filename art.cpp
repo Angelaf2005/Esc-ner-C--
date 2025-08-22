94a9891ebc4cdb9744277cd020ec4215698838a2
Angel Adrian Alvarez Flores <angel.alvarezf@uanl.edu.mx>
2025-08-21 23:03:47 -0600

    Arte del programa
    Solo arte, nada funcional, estaré trabajando en ello.

---
+++ art.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+/// se incluirá la cabecera 
+
+void visualizacion_inicio(){
+    std::cout << R"(
+     ____  ____  ____ _____ ____  ____ ____  _      _      _____ ____ 
+    /  __\/  _ \/  __Y__ __Y ___\/   _Y  _ \/ \  /|/ \  /|/  __//  __\
+    |  \/|| / \||  \/| / \ |    \|  / | / \|| |\ ||| |\ |||  \  |  \/|
+    |  __/| \_/||    / | | \___ ||  \_| |-||| | \||| | \|||  /_ |    /
+    \_/   \____/\_/\_\ \_/ \____/\____|_/ \|\_/  \|\_/  \|\____\\_/\_\
+                                                                  
+    Developed by:
+    - Angel Adrian Alvarez Flores
+    
+    )" << std::endl;
+
+
+
+} /// poner los nombres aquí.
+
