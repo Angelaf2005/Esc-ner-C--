99469e44b7cce8b1a19e608a21d25f6028b2e50b
Kenneth Martinez <145396445+RedKnight023@users.noreply.github.com>
2025-09-06 17:23:16 -0600

    Update main.cpp
    Ahora imprime un mensaje diferente dependiendo del caso que se seleccione

--- src/main.cpp
+++ src/main.cpp
@@ -12,8 +12,29 @@ int main() {
     Dir_escaneo = obtener_datos();
 
 	// 0 - Rango  1 - Lista  2 - Top ports  3 - Todos
-    cout << "Escaneando " << Dir_escaneo->ip << " desde el puerto " << Dir_escaneo->puerto_inicial << " hasta " << Dir_escaneo->puerto_final << "...\n";
-    scan(Dir_escaneo->ip, Dir_escaneo->modo, Dir_escaneo->puerto_inicial, Dir_escaneo->puerto_final, Dir_escaneo->puertos, Dir_escaneo->filename);
+    switch (Dir_escaneo->modo) {
+		case 0: // Rango
+		    cout << " desde el puerto " << Dir_escaneo->puerto_inicial
+		        << " hasta " << Dir_escaneo->puerto_final;
+		    break;
+		case 1: // Lista
+		    cout << " en los puertos: ";
+		    for (size_t i = 0; i < Dir_escaneo->puertos.size(); ++i) {
+		        cout << Dir_escaneo->puertos[i];
+		        if (i < Dir_escaneo->puertos.size() - 1)
+		            cout << ",";
+		    }
+		    break;
+		case 2: // Puertos top
+		    cout << " en puertos comunes: 20,21,22,23,25,53,67,68,80,110,143,443,3306,3389,5900,8080";
+		    break;
+		case 3: // Todos
+		    cout << " en todos los puertos";
+		    break;
+		}
+		
+	cout << "..." << endl;
+	scan(Dir_escaneo->ip, Dir_escaneo->modo, Dir_escaneo->puerto_inicial, Dir_escaneo->puerto_final, Dir_escaneo->puertos, Dir_escaneo->filename);
     cout << "Escaneo terminado." << endl;
 
     delete Dir_escaneo;
